16cbcc5a7570dc2d0005aa6b5c55b714fb40a603
majewskibartosz <majewskibartosz@pm.me>
2023-03-27 00:18:42 +0200

    Update code for step component file

---
+++ src/components/Step.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled from 'styled-components';
+
+type StepProps = {
+  isActive: boolean;
+  isBeat: boolean;
+  handleClick: () => void;
+  handleDoubleClick: () => void;
+};
+
+const StepButton = styled.button<{ isActive: boolean; isBeat: boolean }>`
+  width: 1.5rem;
+  height: 1.5rem;
+  border: none;
+  border-radius: 50%;
+  margin: 0.25rem;
+  background-color: ${({ isActive, isBeat }) =>
+    isActive ? (isBeat ? '#ff8c00' : '#FFD152') : '#282c34'};
+  transition: all 0.3s ease-in-out;
+
+  &:hover {
+    background-color: ${({ isActive, isBeat }) =>
+      isActive ? (isBeat ? '#b36b00' : '#c5a523') : '#5c5f66'};
+  }
+`;
+
+const Step: React.FC<StepProps> = ({ isActive, isBeat, handleClick, handleDoubleClick }) => {
+  return (
+    <StepButton
+      isActive={isActive}
+      isBeat={isBeat}
+      onClick={handleClick}
+      onDoubleClick={handleDoubleClick}
+    />
+  );
+};
+
+export default Step;
